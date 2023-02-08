f601333d6b3ac296175d73b58d632f45d1cc81cc
inho-park <ttotto0608@naver.com>
2023-02-08 16:24:35 +0900

    feat : basic setting for querydsl

---
+++ src/main/java/org/zerock/guestbook/BootGuestbookApplication.java
@@ -0,0 +1,13 @@
+package org.zerock.guestbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootGuestbookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootGuestbookApplication.class, args);
+    }
+
+}
