f3cbb83b1f456daad09d39016792097c1276425b
inho-park <ttotto0608@naver.com>
2023-02-12 23:54:27 +0900

    feat : add register book function

--- src/main/java/org/zerock/guestbook/controller/GuestbookController.java
+++ src/main/java/org/zerock/guestbook/controller/GuestbookController.java
@@ -5,7 +5,10 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.zerock.guestbook.dto.GuestbookDTO;
 import org.zerock.guestbook.dto.PageRequestDTO;
 import org.zerock.guestbook.service.GuestbookService;
 
@@ -29,5 +32,17 @@ public class GuestbookController {
         model.addAttribute("result",  guestbookService.getList(pageRequestDTO));
     }
 
+    // 책 등록 매핑 주소
+    @GetMapping("/register")
+    public void register() {
+        log.info("register get ................................................");
+    }
+    @PostMapping("/register")
+    public String registerPost(GuestbookDTO dto, RedirectAttributes redirectAttributes) {
+        log.info("dto...................................." + dto);
+        Long gno = guestbookService.register(dto);
+        redirectAttributes.addFlashAttribute("msg",gno);
 
+        return "redirect:/guestbook/list";
+    }
 }
